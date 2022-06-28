2eaf25c6b7612946950d8b7ac017836b9d7289b6
Max <max@o0o>
2022-06-28 15:15:00 +0200

    .

--- src/bsp.rs
+++ src/bsp.rs
@@ -1,37 +1,126 @@
+use obj::Obj;
+use std::rc::Rc;
 use crate::vector::Vec3;
 
-pub struct BSPTree<'a> {
-    pub faces: Vec<[[f64; 3]; 3]>,
-    pub infront: Option<&'a mut BSPTree<'a>>,
-    pub behind:  Option<&'a mut BSPTree<'a>>,
+#[derive(Debug)]
+pub struct BSPTree {
+    pub faces: Vec<[Vec3; 3]>,
+    pub infront: Option<Box<BSPTree>>,
+    pub behind:  Option<Box<BSPTree>>,
 }
 
-impl BSPTree<'_> {
-    pub fn add_face(&mut self, camera_origin: &Vec3, hyperplane_normal: &Vec3, face: [[f64; 3]; 3]) {
-        let d = -(hyperplane_normal.x * self.faces[0][0][0] +  hyperplane_normal.y * self.faces[0][0][1] +  hyperplane_normal.z * self.faces[0][0][2]);
+impl BSPTree {
+    pub fn build_new(object: Obj) -> BSPTree {
+        let mut faces = Vec::new();
 
-        let side_f0 = plane_equation(hyperplane_normal, d, face[0]);
-        let side_f1 = plane_equation(hyperplane_normal, d, face[1]);
-        let side_f2 = plane_equation(hyperplane_normal, d, face[2]);
 
-        if side_f0 > 0.0 && side_f1 > 0.0 && side_f2 > 0.0 {
-            // behind?
-            match self.infront {
-                Some(infront) => {
-                    infront.faces.push(face);
-                },
-                None => {
-                    println!("Hi");
-                }
-            }
-        } else if side_f0 < 0.0 && side_f1 < 0.0 && side_f2 < 0.0 {
-            // infront? 
+        for indices in object.indices.chunks(3) {
+            let v0 = Vec3::new(
+                object.vertices[indices[0] as usize].position[0].into(),
+                object.vertices[indices[0] as usize].position[1].into(),
+                object.vertices[indices[0] as usize].position[2].into(),
+            );
+            let v1 = Vec3::new(
+                object.vertices[indices[1] as usize].position[0].into(),
+                object.vertices[indices[1] as usize].position[1].into(),
+                object.vertices[indices[1] as usize].position[2].into(),
+            );
+            let v2 = Vec3::new(
+                object.vertices[indices[2] as usize].position[0].into(),
+                object.vertices[indices[2] as usize].position[1].into(),
+                object.vertices[indices[2] as usize].position[2].into(),
+            );
+
+            faces.push([v0, v1, v2]);
+        }
+
+        let tree = BSPTree {faces, infront: None, behind: None };
+        tree
+    }
+
+    pub fn build(parent: &mut Box<BSPTree>, depth: i32) {
+        if depth < 0 {
+            return;
+        }
+
+        let parent_v0 = Vec3::copy(&parent.faces[0][0]);
+        let parent_v1 = Vec3::copy(& parent.faces[0][1]);
+        let parent_v2 = Vec3::copy(&parent.faces[0][2]);
+        let p0p1 = &parent_v1 - &parent_v0;
+        let p0p2 = &parent_v2 - &parent_v0;
+        let hyperplane_normal = p0p1.cross(&p0p2);
+
+        if parent.faces.len() < 2 { return; };
+        for i in 1..(parent.faces.len()-1) {
+            let v0 = Vec3::new(parent.faces[i][0].x, parent.faces[i][0].y, parent.faces[i][0].z);
+            let v1 = Vec3::new(parent.faces[i][1].x, parent.faces[i][1].y, parent.faces[i][1].z);
+            let v2 = Vec3::new(parent.faces[i][2].x, parent.faces[i][2].y, parent.faces[i][2].z);
+
+            parent.add_face(&hyperplane_normal, [v0, v1, v2]);
+        }
+        // TODO need to remove items / keep items based on whether they were added in add_face - shouldn't remove
+        // items on the same plane
+        parent.faces = vec![[Vec3::copy(&parent.faces[0][0]), Vec3::copy(&parent.faces[0][1]), Vec3::copy(&parent.faces[0][2])]];
+
+        if let Some(child_infront) = &mut parent.infront {
+            BSPTree::build(child_infront, depth-1);
         } else {
-            // intersection ?
+            println!("No children infront?");
+        }
+
+        if let Some(child_behind) = &mut parent.behind {
+            BSPTree::build(child_behind, depth-1);
+        } else {
+            println!("No children behind?");
         }
     }
+
+    pub fn new() -> BSPTree {
+        BSPTree { faces: vec![], infront: None, behind: None }
+    }
+
+    pub fn add_face(&mut self, hyperplane_normal: &Vec3, face: [Vec3; 3]) {
+        let d = -(hyperplane_normal.x * self.faces[0][0].x +  hyperplane_normal.y * self.faces[0][0].y +  hyperplane_normal.z * self.faces[0][0].z);
+
+        let side_f0 = plane_equation(hyperplane_normal, d, &face[0]);
+        let side_f1 = plane_equation(hyperplane_normal, d, &face[1]);
+        let side_f2 = plane_equation(hyperplane_normal, d, &face[2]);
+
+        if side_f0 > 0.0 && side_f1 > 0.0 && side_f2 > 0.0 {
+            // Behind hyperplane
+            if let Some(infront) = &mut self.infront {
+                infront.faces.push(face);
+                return;
+            }
+            let mut infront_branch = Box::new(BSPTree::new());
+            infront_branch.faces.push(face);
+            self.infront = Some(infront_branch);
+
+        } else if side_f0 < 0.0 && side_f1 < 0.0 && side_f2 < 0.0 {
+            // Infront of hyperplane
+            if let Some(behind) = &mut self.behind {
+                behind.faces.push(face);
+                return;
+            }
+            let mut behind_branch = Box::new(BSPTree::new());
+            behind_branch.faces.push(face);
+            self.infront = Some(behind_branch);
+
+        } else if side_f0 == 0.0 && side_f1 == 0.0 && side_f2 == 0.0 {
+            // On the plane
+            // self.faces.push(face);
+            // Do nothing because already in right place?
+        }  else {
+            // Intersection of hyperplane
+            eprintln!("Intersection");
+        }
+    }
+
+    // pub fn ray_insection_face(&self) -> [[f64; 3]; 3] {
+        
+    // }
 }
 
-fn plane_equation(normal: &Vec3, d: f64, point: [f64; 3]) -> f64 {
-    normal.x * point[0] + normal.y * point[1] + normal.z * point[2] + d
+fn plane_equation(normal: &Vec3, d: f64, point: &Vec3) -> f64 {
+    normal.x * point.x + normal.y * point.y + normal.z * point.z + d
 }
--- src/object.rs
+++ src/object.rs
@@ -5,17 +5,20 @@ use crate::material::Material;
 use crate::ray::Ray;
 use crate::vector::Vec3;
 use crate::utils::{distance};
+use crate::bsp::BSPTree;
 use obj::Obj;
 use std::sync::{Arc, Mutex};
 
 pub struct Object<M: Material> {
-    object: Obj,
+    pub tree: Box<BSPTree>,
     material: M,
 }
 
 impl<M: Material> Object<M> {
     pub fn new(object: Obj, material: M) -> Object<M> {
-        Object { object, material }
+        let mut tree = Box::new(BSPTree::build_new(object));
+        BSPTree::build(&mut tree, 1);
+        Object { tree, material }
     }
 }
 
@@ -24,91 +27,91 @@ impl<T: Material> Hittable for Object<T> {
         let mut potential_hit: Option<HitRecord> = None;
         let mut _position = Vec3::new(INFINITY, INFINITY, INFINITY);
 
-        for indices in self.object.indices.chunks(3) {
-            let v_0 = &self.object.vertices[indices[0] as usize];
-            let v_1 = &self.object.vertices[indices[1] as usize];
-            let v_2 = &self.object.vertices[indices[2] as usize];
+        // for indices in self.object.indices.chunks(3) {
+        //     let v_0 = &self.object.vertices[indices[0] as usize];
+        //     let v_1 = &self.object.vertices[indices[1] as usize];
+        //     let v_2 = &self.object.vertices[indices[2] as usize];
 
-            let p1 = Vec3::new(
-                v_0.position[0].into(),
-                v_0.position[1].into(),
-                v_0.position[2].into(),
-            );
-            let p2 = Vec3::new(
-                v_1.position[0].into(),
-                v_1.position[1].into(),
-                v_1.position[2].into(),
-            );
-            let p3 = Vec3::new(
-                v_2.position[0].into(),
-                v_2.position[1].into(),
-                v_2.position[2].into(),
-            );
+        //     let p1 = Vec3::new(
+        //         v_0.position[0].into(),
+        //         v_0.position[1].into(),
+        //         v_0.position[2].into(),
+        //     );
+        //     let p2 = Vec3::new(
+        //         v_1.position[0].into(),
+        //         v_1.position[1].into(),
+        //         v_1.position[2].into(),
+        //     );
+        //     let p3 = Vec3::new(
+        //         v_2.position[0].into(),
+        //         v_2.position[1].into(),
+        //         v_2.position[2].into(),
+        //     );
 
-            let p1p2 = &p2 - &p1;
-            let p1p3 = &p3 - &p1;
-            let n = p1p2.cross(&p1p3);
+        //     let p1p2 = &p2 - &p1;
+        //     let p1p3 = &p3 - &p1;
+        //     let n = p1p2.cross(&p1p3);
 
-            let triangle_ray_dot_product = n.dot(&ray.direction);
-            if triangle_ray_dot_product.abs() == 0.0 {
-                continue;
-            }
+        //     let triangle_ray_dot_product = n.dot(&ray.direction);
+        //     if triangle_ray_dot_product.abs() == 0.0 {
+        //         continue;
+        //     }
 
-            let d = -n.dot(&p1);
+        //     let d = -n.dot(&p1);
 
-            let t = -(n.dot(&ray.origin) + d) / triangle_ray_dot_product;
-            if t < 0.0 {
-                continue;
-            }
+        //     let t = -(n.dot(&ray.origin) + d) / triangle_ray_dot_product;
+        //     if t < 0.0 {
+        //         continue;
+        //     }
 
-            let p = ray.at(t);
+        //     let p = ray.at(t);
 
-            let edge0 = &p2 - &p1;
-            let v_p1 = &p - &p1;
-            let c0 = edge0.cross(&v_p1);
-            if n.dot(&c0) < 0.0 {
-                continue;
-            }
+        //     let edge0 = &p2 - &p1;
+        //     let v_p1 = &p - &p1;
+        //     let c0 = edge0.cross(&v_p1);
+        //     if n.dot(&c0) < 0.0 {
+        //         continue;
+        //     }
 
-            let edge1 = &p3 - &p2;
-            let v_p2 = &p - &p2;
-            let c1 = edge1.cross(&v_p2);
-            if n.dot(&c1) < 0.0 {
-                continue;
-            }
+        //     let edge1 = &p3 - &p2;
+        //     let v_p2 = &p - &p2;
+        //     let c1 = edge1.cross(&v_p2);
+        //     if n.dot(&c1) < 0.0 {
+        //         continue;
+        //     }
 
-            let edge2 = p1 - &p3;
-            let v_p3 = &p - &p3;
-            let c2 = edge2.cross(&v_p3);
-            if n.dot(&c2) < 0.0 {
-                continue;
-            }
+        //     let edge2 = p1 - &p3;
+        //     let v_p3 = &p - &p3;
+        //     let c2 = edge2.cross(&v_p3);
+        //     if n.dot(&c2) < 0.0 {
+        //         continue;
+        //     }
 
-            let n_norm = n.unit();
-            let mut _front_face = true;
-            if ray.direction.dot(&n_norm) > 0.0 {
-                _front_face = false;
-                continue;
-            }
+        //     let n_norm = n.unit();
+        //     let mut _front_face = true;
+        //     if ray.direction.dot(&n_norm) > 0.0 {
+        //         _front_face = false;
+        //         continue;
+        //     }
 
-            let cam_look_from = Vec3::new(8.0, 2.0, 2.0);
-            let z_distance = distance(&p, &cam_look_from).abs();
-            let mut zbuff = zbuffer.lock().unwrap();
+        //     let cam_look_from = Vec3::new(8.0, 2.0, 2.0);
+        //     let z_distance = distance(&p, &cam_look_from).abs();
+        //     let mut zbuff = zbuffer.lock().unwrap();
             
-            if z_distance < zbuff[p_0 as usize][p_1 as usize] {
-                zbuff[p_0 as usize][p_1 as usize] = z_distance;
-            } else {
-                continue;
-            }
+        //     if z_distance < zbuff[p_0 as usize][p_1 as usize] {
+        //         zbuff[p_0 as usize][p_1 as usize] = z_distance;
+        //     } else {
+        //         continue;
+        //     }
 
-            potential_hit = Some(HitRecord {
-                p,
-                t,
-                normal: n_norm,
-                material: &self.material,
-                front_face: _front_face,
-            });
-        }
+        //     potential_hit = Some(HitRecord {
+        //         p,
+        //         t,
+        //         normal: n_norm,
+        //         material: &self.material,
+        //         front_face: _front_face,
+        //     });
+        // }
 
         // reset so sampling works correctly
         let mut zbuff = zbuffer.lock().unwrap();
